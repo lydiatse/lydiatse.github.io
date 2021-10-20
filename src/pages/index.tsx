c97fb263842b55aee26c2ee4f281f1e566b8c85d
Lydia Tse <lydiat78@gmail.com>
2021-10-19 23:08:01 -0500

    🏠 Created homepage (#1)
    * Updated linters
    
    * Added precommit hook
    
    * Added lint staged
    
    * Basic homepage created
    
    * Updated hero image
    
    Co-authored-by: Lydia Tse <lydia@Lydias-MacBook-Pro-5.local>

---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import { Box, Grid, Typography } from '@mui/material';
+import Head from 'next/head';
+import Image from 'next/image';
+import { Layout } from '../components';
+import heroPic from '../../public/hero.png';
+
+export default function Home() {
+  return (
+    <Layout>
+      <Grid
+        container
+        alignItems="center"
+        alignContent="center"
+        justifyContent="center"
+        spacing={8}
+      >
+        <Grid item>
+          <Box
+            sx={{
+              height: 500,
+              mx: '2rem',
+              width: 500,
+            }}
+          >
+            <Image src={heroPic} alt="Photo of Lydia" />
+          </Box>
+        </Grid>
+        <Grid item>
+          <Typography variant="h2">Hi there! I'm a</Typography>
+          <Typography variant="h2">Computer Scientist,</Typography>
+          <Typography variant="h2">Teacher &amp;</Typography>
+          <Typography variant="h2">Lifelong Learner.</Typography>
+        </Grid>
+      </Grid>
+    </Layout>
+  );
+}
