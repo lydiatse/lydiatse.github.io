7b73e2ed058b4d36eac9280c82f487caae44c850
Lydia Tse <lydia@Lydias-MacBook-Pro-5.local>
2021-10-19 23:18:24 -0500

    Resolved TS build errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import {AppProps} from 'next/app'
-import '@fontsource/cabin/400.css'
-import '@fontsource/cabin/700.css'
+import React from 'react';
+import { AppProps } from 'next/app';
+import '@fontsource/cabin/400.css';
+import '@fontsource/cabin/700.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
