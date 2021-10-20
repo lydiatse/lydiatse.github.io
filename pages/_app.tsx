4a98a6b23468b202d9a7119987e6bae30ad34545
Lydia Tse <lydia@Lydias-MacBook-Pro-5.local>
2021-10-19 20:49:05 -0500

    Updated linters

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css'
 import {AppProps} from 'next/app'
+import '@fontsource/cabin/400.css'
+import '@fontsource/cabin/700.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
