21c67b08995f99373dc73bdd2b96f36aacd4644e
Lydia Tse <lydia@Lydias-MacBook-Pro-5.local>
2021-10-19 23:06:02 -0500

    Updated hero image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Box, Grid, Typography } from '@mui/material';
 import Head from 'next/head';
 import Image from 'next/image';
 import { Layout } from '../components';
+import heroPic from '../../public/hero.png';
 
 export default function Home() {
   return (
@@ -16,18 +17,16 @@ export default function Home() {
         <Grid item>
           <Box
             sx={{
-              backgroundColor: '#7299db',
-              borderRadius: '50%',
               height: 500,
               mx: '2rem',
               width: 500,
             }}
           >
-            <Image src="/hero.png" alt="me" width="200" height="300" />
+            <Image src={heroPic} alt="Photo of Lydia" />
           </Box>
         </Grid>
         <Grid item>
-          <Typography variant="h2">Hi there, I'm a</Typography>
+          <Typography variant="h2">Hi there! I'm a</Typography>
           <Typography variant="h2">Computer Scientist,</Typography>
           <Typography variant="h2">Teacher &amp;</Typography>
           <Typography variant="h2">Lifelong Learner.</Typography>
