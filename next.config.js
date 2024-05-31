fbec3536c4aa2a0d7a46f93a22608a698a89a539
lydiatse <lydiahtse@gmail.com>
2024-05-30 19:18:16 -0500

    Upgrade to Next.js v14 and app router.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-const withImages = require('next-images');
-module.exports = withImages({
-  reactStrictMode: true,
-  images: {
-    disableStaticImages: true,
-  },
-});
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+};
+
+module.exports = nextConfig;
